9ff202f2a0175d79951c921ef0ec6bb6ed3d7d82
Ahmed Atef <atefa1476@gmail.com>
2022-10-10 21:40:26 +0200

    V1.2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,97 @@
-const submit = document.getElementById("submit");
 const input = document.getElementById("input");
+const submit = document.getElementById("submit");
 const error = document.getElementById("error");
+const outputA = document.getElementById("outputA");
+const outputB = document.getElementById("outputB");
 
 const showError = ( message ) => {
     if (error.classList.contains("hidden")) error.classList.remove("hidden");
-    error.innerHTML = message;
+    if (!outputA.classList.contains("hidden")) outputA.classList.add("hidden");
+    if (!outputB.classList.contains("hidden")) outputB.classList.add("hidden");
+    error.innerHTML = "*" + message;
+    outputA.innerHTML = "";
+    outputB.innerHTML = "";
 };
 
-submit.addEventListener("click", () => {
-    if (input.value === "") {
-        showError("*الحقل فارغ");
+const customTrim = ( phrase ) => {
+    return phrase.trim().replace(/\s+/g, " ");;
+};
+
+const analyze = ( userInput ) => {
+    if (userInput === "") {
+        showError("الحقل فارغ");
+    } else if (userInput.length > 2048) {
+        showError("يرجى إدخال عدد حروف أقل");
     } else {
+
         if (!error.classList.contains("hidden")) error.classList.add("hidden");
+        error.innerHTML = "";
+
+        // Validate Arabic chars and formations.
+        let userAlphabets = "";
+        let userFormatedAlphabets = "";
+        for ( let char of userInput ) {
+            if (ALPHABETS.includes(char) || char === " ") {
+                userAlphabets += char;
+                userFormatedAlphabets += char;
+            } else if (FORMATIONS.includes(char)) {
+                userFormatedAlphabets += char;
+            }
+        }
+
+        // Remove unwanted spaces.
+        userAlphabets = customTrim(userAlphabets);
+        userFormatedAlphabets = customTrim(userFormatedAlphabets);
+
+        if (!userAlphabets.length) {
+            showError("لم يتم العثور على أحرف عربية!");
+            return;
+        }
+
+        input.value = userFormatedAlphabets;
+
+        let outputAlphabets = "";
+        let outputBinary = "";
+
+        for ( let i in userFormatedAlphabets ) {
+            i = parseInt(i);
+            let charBefore = userFormatedAlphabets[i-1];
+            let char = userFormatedAlphabets[i];
+            let charAfter = userFormatedAlphabets[i+1];
+            if ( ALPHABETS.includes(char) || char === " " ) {
+                if ( char === " " ) {
+                    outputAlphabets += " ";
+                    outputBinary += " ";
+                } else if ( !charBefore ) {
+                    // First char.
+                    outputAlphabets += char;
+                    outputBinary += "/";
+                } else if ( SILENT_FORMATION === charAfter ) {
+                    outputAlphabets += char;
+                    outputBinary += "O";
+                } else if ( VOWEL_FORMATIONS.includes(charAfter) ) {
+                    outputAlphabets += char;
+                    outputBinary += "/";
+                } else {
+                    // Couldn't detect.
+                    outputAlphabets += `<span class="notice">${char}</span>`;
+                    outputBinary += '<span class="notice">_</span>';
+                }
+            }
+        }
+
+        if ( outputAlphabets ) {
+            outputA.innerHTML = outputAlphabets;
+            if (outputA.classList.contains("hidden")) outputA.classList.remove("hidden");
+        }
+
+        if ( outputBinary.trim() ) {
+            outputB.innerHTML = outputBinary;
+            if (outputB.classList.contains("hidden")) outputB.classList.remove("hidden");
+        }
+
     }
-});
+};
+
+submit.addEventListener("click", () => { analyze(input.value); });
+input.addEventListener("keypress", (e) => { if (e.key === "Enter") analyze(e.target.value); });
